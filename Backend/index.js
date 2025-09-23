d6dfb03ea7a540a305a4d61aae8c660ef20cbe10
vaibhav verma <vaibhavverma6153@gmail.com>
2025-09-23 20:07:59 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -28,6 +28,15 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// Serve static files from the React frontend app
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve();
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req, res) => {  
+    res.sendFile(path.join(dirPath, "./Frontend/dist","index.html"));
+})};
+
 
 server.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`);
